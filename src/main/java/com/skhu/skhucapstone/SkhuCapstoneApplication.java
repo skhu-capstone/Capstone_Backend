48d05198e26f03065fc93e17c69a16fb61042777
daun <wjdekdns0218@gmail.com>
2026-04-05 20:16:33 +0900

    Initial commit

---
+++ src/main/java/com/skhu/skhucapstone/SkhuCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.skhu.skhucapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skhu/skhucapstone/SkhuCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.skhucapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkhuCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
