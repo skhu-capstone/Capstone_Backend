29f601e5368bd7366957e53ab0be212c3d0b21da
shtfh <syj2003826@gmail.com>
2026-04-19 22:22:21 +0900

    fefeat(usai ClubCreateRequest DTO 추가

---
+++ src/main/java/com/skhu/skhucapstone/club/api/dto/Response/ClubResponse.java
@@ -0,0 +1,25 @@
+package com.skhu.skhucapstone.club.api.dto.Response;
+
+import com.skhu.skhucapstone.club.domain.Club;
+
+import java.time.LocalDateTime;
+
+public record ClubResponse(
+        Long id,
+        String clubName,
+        String description,
+        Boolean isApproved,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static ClubResponse from(Club club) {
+        return new ClubResponse(
+                club.getId(),
+                club.getClubName(),
+                club.getDescription(),
+                club.getIsApproved(),
+                club.getCreatedAt(),
+                club.getUpdatedAt()
+        );
+    }
+}
