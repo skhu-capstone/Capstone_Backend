6209a11cca9e693f88a81639f2470bde704721bb
shtfh <syj2003826@gmail.com>
2026-04-19 22:04:36 +0900

    feat(user): ClubRepository 추가

---
+++ src/main/java/com/skhu/skhucapstone/club/domain/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package com.skhu.skhucapstone.club.domain.repository;
+
+import com.skhu.skhucapstone.club.domain.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
