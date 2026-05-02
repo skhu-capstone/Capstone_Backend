a1a8eaaa706a0ca243c62ce44187115585692a50
daun <wjdekdns0218@gmail.com>
2026-05-02 13:29:06 +0900

    feat: 학교 이메일 인증 엔드포인트 구현 #4

--- src/main/java/com/skhu/skhucapstone/auth/controller/AuthController.java
+++ src/main/java/com/skhu/skhucapstone/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.skhu.skhucapstone.auth.controller;
 
-import com.skhu.skhucapstone.auth.dto.GoogleLoginReq;
-import com.skhu.skhucapstone.auth.dto.GoogleLoginRes;
+import com.skhu.skhucapstone.auth.dto.*;
 import com.skhu.skhucapstone.auth.service.AuthService;
 import com.skhu.skhucapstone.common.exception.SuccessCode;
 import com.skhu.skhucapstone.common.response.ApiResponse;
@@ -10,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +31,34 @@ public class AuthController {
             GoogleLoginReq req) {
         GoogleLoginRes res = authService.googleLogin(req.getGoogleAccessToken());
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.LOGIN_SUCCESS, res));
+    }
+
+    @PostMapping("/email/send")
+    @Operation(summary = "학교 이메일 인증번호 발송", description = "학교 이메일로 인증번호를 발송합니다.")
+    public ResponseEntity<ApiResponse<EmailSendRes>> sendVerificationEmail(
+            @RequestBody @Valid EmailSendReq req,
+            @AuthenticationPrincipal Long userId
+    ){
+        authService.sendVerificationEmail(req.getSchoolEmail(), userId);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.EMAIL_SEND_SUCCESS, new EmailSendRes(req.getSchoolEmail())));
+    }
+
+    @PostMapping("/email/verify")
+    @Operation(summary = "학교 이메일 인증번호 확인", description = "인증번호를 확인하고 학교 이메일 인증을 완료합니다.")
+    public ResponseEntity<ApiResponse<EmailVerifyRes>> verifyEmail(
+            @RequestBody @Valid EmailVerifyReq req,
+            @AuthenticationPrincipal Long userId
+    ) {
+        authService.verifyEmail(req.getSchoolEmail(), req.getCode(), userId);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.EMAIL_VERIFY_SUCCESS, new EmailVerifyRes(userId, req.getSchoolEmail(), true)));
+    }
+
+    @PostMapping("/email/resend")
+    @Operation(summary = "학교 이메일 인증번호 재발송", description = "인증번호를 재발송합니다.")
+    public ResponseEntity<ApiResponse<EmailSendRes>> resendVerificationEmail(
+            @RequestBody @Valid EmailSendReq req,
+            @AuthenticationPrincipal Long userId) {
+        authService.resendVerificationEmail(req.getSchoolEmail(), userId);
+        return ResponseEntity.ok(ApiResponse.success(SuccessCode.EMAIL_RESEND_SUCCESS, new EmailSendRes(req.getSchoolEmail())));
     }
 }
