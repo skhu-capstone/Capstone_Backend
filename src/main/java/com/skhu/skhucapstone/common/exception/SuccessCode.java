1b90be0ef1ac3d3f66fae66efbf7591e57eb5587
daun <wjdekdns0218@gmail.com>
2026-05-02 13:27:05 +0900

    feat(AI): 이메일 인증 성공 코드 추가 #4

--- src/main/java/com/skhu/skhucapstone/common/exception/SuccessCode.java
+++ src/main/java/com/skhu/skhucapstone/common/exception/SuccessCode.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessCode {
 
-    LOGIN_SUCCESS(HttpStatus.OK, "LOGIN_SUCCESS", "구글 소셜 로그인이 완료되었습니다.");
+    LOGIN_SUCCESS(HttpStatus.OK, "LOGIN_SUCCESS", "구글 소셜 로그인이 완료되었습니다."),
+
+    // 인증번호
+    EMAIL_SEND_SUCCESS(HttpStatus.OK, "EMAIL_SEND_SUCCESS", "인증번호가 발송되었습니다."),
+    EMAIL_VERIFY_SUCCESS(HttpStatus.OK, "EMAIL_VERIFY_SUCCESS", "학교 이메일 인증이 완료되었습니다."),
+    EMAIL_RESEND_SUCCESS(HttpStatus.OK, "EMAIL_RESEND_SUCCESS", "인증번호가 재발송되었습니다.");
 
     private final HttpStatus status;
     private final String code;
